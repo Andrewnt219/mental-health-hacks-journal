7a02b618ddc2a5321ddce5e5fd614a3638c15b11
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-06-06 09:44:28 -0400

    add weekly tracker route

--- back-end/functions/routes/index.js
+++ back-end/functions/routes/index.js
@@ -5,6 +5,7 @@ const Feedback = require("./feedback/index");
 const Journal = require("./journal/index");
 const Prompt = require("./prompt/index");
 const User = require("./user/index");
+const WeeklyTracker = require('./weeklyTracker/index');
 
 router.use(cors());
 
@@ -12,5 +13,6 @@ router.use("/", Feedback);
 router.use("/", Journal);
 router.use("/", Prompt);
 router.use("/", User);
+router.use('/', WeeklyTracker);
 
 module.exports = router;
